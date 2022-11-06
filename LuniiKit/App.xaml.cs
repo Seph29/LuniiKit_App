e43612a35ba6fd41c61067c31ea1f679d1e0fe3e
Seph29 <dev@flemanach.tech>
2022-11-06 14:52:00 +0100

    2.2.7.1

--- LuniiKit/App.xaml.cs
+++ LuniiKit/App.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows;
+using WPFCustomMessageBox;
 
 namespace LuniiKit
 {
@@ -7,5 +12,23 @@ namespace LuniiKit
     /// </summary>
     public partial class App : Application
     {
+        private static System.Threading.Mutex _mutex = null;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            string mutexId = ((System.Runtime.InteropServices.GuidAttribute)System.Reflection.Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(System.Runtime.InteropServices.GuidAttribute), false).GetValue(0)).Value.ToString();
+            _mutex = new System.Threading.Mutex(true, mutexId, out bool createdNew);
+            if (!createdNew)
+            {
+                CustomMessageBox.Show(Application.Current.MainWindow, "Une instance de LuniiKit est déjà en cours d'exécution.", "LuniiKit", MessageBoxButton.OK, MessageBoxImage.Error);
+                Current.Shutdown();
+            }
+            else Exit += CloseMutexHandler;
+            base.OnStartup(e);
+        }
+        protected virtual void CloseMutexHandler(object sender, EventArgs e)
+        {
+            _mutex?.Close();
+        }
     }
 }
